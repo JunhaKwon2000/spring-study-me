7fdffe4a368591c0ca0afe497dd37fdfb884f90f
JunhaKwon2000 <junharoket@naver.com>
2025-07-31 12:16:37 +0900

    [권준하] 스타일 작업 수업(부트스트랩)
    * index 및 include 작성 + 부트스트랩 템플릿 적용

--- src/main/java/com/winter/app/board/BoardDAO.java
+++ src/main/java/com/winter/app/board/BoardDAO.java
@@ -4,7 +4,10 @@ public interface BoardDAO {
 	
 	// insert
 	public int insert(BoardVO boardVO) throws Exception;
+	
 	public int update(BoardVO boardVO) throws Exception;
+	
 	public int delete(BoardVO boardVO) throws Exception;
+	
 	public BoardVO detail(BoardVO boardVO) throws Exception;
 }
