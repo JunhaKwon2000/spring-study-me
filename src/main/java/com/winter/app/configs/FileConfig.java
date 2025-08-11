fe7ff629d48f64f8b77daad447fcb4c446244bb3
JunhaKwon2000 <junharoket@naver.com>
2025-08-11 17:54:19 +0900

    커밋

--- src/main/java/com/winter/app/configs/FileConfig.java
+++ src/main/java/com/winter/app/configs/FileConfig.java
@@ -16,6 +16,6 @@ public class FileConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(url).addResourceLocations("file:\\" + path);
+		registry.addResourceHandler(url).addResourceLocations("file:" + path);
 	}
 }
