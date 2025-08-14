4556613cf5abd3b2fcecd0fce965a9dd97d40224
JunhaKwon2000 <junharoket@naver.com>
2025-08-14 18:28:04 +0900

    [권준하] 언어 설정 커밋
    * 언어 설정을 작업했습니다.

---
+++ src/main/java/com/winter/app/configs/MessageConfig.java
@@ -0,0 +1,52 @@
+package com.winter.app.configs;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class MessageConfig implements WebMvcConfigurer {
+	
+	@Bean
+	LocaleResolver localeResolver() {
+		
+		// 언어 정보를 계속해서 웹 브라우저를 이용하면서 보관하고 싶을 때 쿠키와 세션이 정답
+		
+		// 1. 만약 session을 이용하고 싶을 경우
+		SessionLocaleResolver resolver = new SessionLocaleResolver();
+		resolver.setDefaultLocale(Locale.KOREAN);
+
+		// 2. 만약 쿠키를 이용하고 싶을 경우
+		CookieLocaleResolver resolver2 = new CookieLocaleResolver("lang");
+		resolver2.setDefaultLocale(Locale.KOREAN);
+		
+		return resolver2; // 현재는 쿠키 사용
+		
+		// 이후 인터셉터에서 html이 구성되고 난 이후 언어를 변환하는 과정을 거침(인터셉터로)
+		
+	}
+	
+	// Message Interceptor를 여기에 바로 선언
+	LocaleChangeInterceptor changeInterceptor() {
+		LocaleChangeInterceptor changeInterceptor = new LocaleChangeInterceptor();
+		
+		changeInterceptor.setParamName("lang");
+		// ex: url?lang=ko
+		
+		return changeInterceptor;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(this.changeInterceptor())
+				.addPathPatterns("/**");
+	}
+	
+}
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,6 +17,16 @@
                 <div class="container-fluid">
 	                <!-- 페이지 본문 -->
 	                <h1 class="h3 mb-4 text-gray-800">Hello World!</h1>
+	                <h3>
+	                	<spring:message code="welcome.message2" text="Welcome" />
+	                </h3>
+	                
+	                <c:if test="${ not empty member }">
+	                	<h3>Welcome, ${ member.name }</h3>
+	                	<h3>
+	                		<spring:message code="user.info" arguments="${ member.username },${ member.email }" argumentSeparator="," />
+	                	</h3>
+	                </c:if>
                 </div>
 			</div>
 			<%@ include file="/WEB-INF/views/include/footer.jsp" %>
